d573f06cec399da7f55fce30d2959dfb95f62063
Michał Borzęcki <ziqzzaq.web@gmail.com>
2018-03-18 12:21:02 +0100

    Added logout method

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private angularFire: AngularFireAuth) { }
 
   login(email: string, password: string) {
     console.log(email,password)
   }
 
   signup(email: string, password: string) {
-
+    this.angularFire.auth.createUserWithEmailAndPassword(email,password).then(value =>{
+      console.log(value);
+    }).catch(err => {
+      console.log(err);
+    })
   }
 
   logout() {
-
+    this.angularFire.auth.signOut();
   }
 }
