0c3271486d9e1fc428b56273852f1384120558f6
Michał Borzęcki <ziqzzaq.web@gmail.com>
2018-04-16 15:56:03 +0200

    Deleted redundant import

--- src/app/components/chat/users-list/users-list.component.ts
+++ src/app/components/chat/users-list/users-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { User, auth } from 'firebase';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../../auth/auth.service';
 
